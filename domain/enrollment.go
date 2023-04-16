61a9db02b58f6c71a9d088b8565b6316268256d8
Jonathan <jonathan.navas9@hotmail.com>
2023-04-16 15:21:21 -0500

    update domain enrollment

--- domain/enrollment.go
+++ domain/enrollment.go
@@ -10,9 +10,9 @@ import (
 type Enrollment struct {
 	ID        string     `json:"id" gorm:"type:char(36);not null;primary_key;unique_index"`
 	UserID    string     `json:"user_id,omitempty" gorm:"type:char(50)"`
-	User      *User      `json:"user,omitempty"`
+	User      *User      `json:"user,omitempty" gorm:"-"`
 	CourseId  string     `json:"course_id,omitempty" gorm:"type:char(50);not null"`
-	Course    *Course    `json:"course,omitempty"`
+	Course    *Course    `json:"course,omitempty" gorm:"-"`
 	Status    string     `json:"status" gorm:"type:char(2)"`
 	CreatedAt *time.Time `json:"-"`
 	UpdatedAt *time.Time `json:"-"`
