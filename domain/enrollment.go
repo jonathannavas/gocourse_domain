53908490534fc27d32bc48839201ee98011276bb
Jonathan <jonathan.navas9@hotmail.com>
2023-04-16 19:32:30 -0500

    update status enrollment

--- domain/enrollment.go
+++ domain/enrollment.go
@@ -8,15 +8,24 @@ import (
 )
 
 type Enrollment struct {
-	ID        string     `json:"id" gorm:"type:char(36);not null;primary_key;unique_index"`
-	UserID    string     `json:"user_id,omitempty" gorm:"type:char(50)"`
-	User      *User      `json:"user,omitempty" gorm:"-"`
-	CourseId  string     `json:"course_id,omitempty" gorm:"type:char(50);not null"`
-	Course    *Course    `json:"course,omitempty" gorm:"-"`
-	Status    string     `json:"status" gorm:"type:char(2)"`
-	CreatedAt *time.Time `json:"-"`
-	UpdatedAt *time.Time `json:"-"`
+	ID        string           `json:"id" gorm:"type:char(36);not null;primary_key;unique_index"`
+	UserID    string           `json:"user_id,omitempty" gorm:"type:char(50)"`
+	User      *User            `json:"user,omitempty" gorm:"-"`
+	CourseId  string           `json:"course_id,omitempty" gorm:"type:char(50);not null"`
+	Course    *Course          `json:"course,omitempty" gorm:"-"`
+	Status    EnrollmentStatus `json:"status" gorm:"type:char(2)"`
+	CreatedAt *time.Time       `json:"-"`
+	UpdatedAt *time.Time       `json:"-"`
 }
+
+type EnrollmentStatus string
+
+const (
+	Pending  EnrollmentStatus = "P"
+	Active   EnrollmentStatus = "A"
+	Studying EnrollmentStatus = "S"
+	Inactive EnrollmentStatus = "I"
+)
 
 func (c *Enrollment) BeforeCreate(tx *gorm.DB) (err error) {
 	if c.ID == "" {
